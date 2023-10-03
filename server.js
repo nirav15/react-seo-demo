6f91fb316125a954a5f7808aaee5fbf36c18de9b
nirav15 <niravkakadiya@logisticinfotech.co.in>
2023-10-03 13:02:54 +0530

    add log

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, './build')));
 
+console.log("yes server running");
 app.get('*', function (request, response) {
     const filePath = path.resolve(__dirname, './build', 'index.html');
     response.sendFile(filePath);
